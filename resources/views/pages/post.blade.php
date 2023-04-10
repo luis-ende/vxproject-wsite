408b7c8224b28c7470c2ff2929b35a532cc1a283
Jose Luis Salazar <lsalazar@gmx.de>
2023-04-10 17:15:24 -0600

    Se implementó nombre de autor en artículo de blog

--- resources/views/pages/post.blade.php
+++ resources/views/pages/post.blade.php
@@ -6,6 +6,8 @@
                 <div class="pt-8 lg:pt-16 px-4 mx-auto max-w-screen-md">
                     <h2 class="mb-4 text-4xl tracking-tight font-extrabold text-center text-gray-900 dark:text-white">{{ $post->title }}</h2>
                     <p class="font-light text-center text-gray-500 dark:text-gray-400 sm:text-xl md:text-base border-b border-t py-2">
+                        @svg('bx-user-pin', ['class' => 'w-6 h-6 inline-block mr-2'])
+                        <span class="mr-5">VX Project</span>
                         @svg('zondicon-calendar', ['class' => 'w-5 h-5 inline-block mr-2'])
                         Marzo 23, 2023
                     </p>
