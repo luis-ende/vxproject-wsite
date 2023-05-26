07447a1293802374beda57e66544710e86d2cd12
Jose Luis Salazar <lsalazar@gmx.de>
2023-05-26 12:26:57 -0600

    Ajuste de tamaño de fuente responsivo para títulos de artículos

--- resources/views/pages/post.blade.php
+++ resources/views/pages/post.blade.php
@@ -4,7 +4,7 @@
         <div class="px-10 md:px-0 flex flex-col items-center">
             <section class="bg-white dark:bg-gray-900">
                 <div class="pt-8 lg:pt-16 px-4 mx-auto max-w-screen-md">
-                    <h2 class="mb-4 text-4xl tracking-tight font-extrabold text-center text-gray-900 dark:text-white">{{ $post->title }}</h2>
+                    <h2 class="mb-4 text-lg md:text-4xl tracking-tight font-extrabold text-center text-gray-900 dark:text-white">{{ $post->title }}</h2>
                     <p class="font-light text-center text-gray-500 dark:text-gray-400 sm:text-xl md:text-base border-b border-t py-2">
                         @svg('bx-user-pin', ['class' => 'w-6 h-6 inline-block mr-2'])
                         <span class="mr-5">Arturo</span>
