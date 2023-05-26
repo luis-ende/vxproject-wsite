86caa6879b293eb1b0cf244fc01c96b40c750bb3
Jose Luis Salazar <lsalazar@gmx.de>
2023-05-26 12:09:30 -0600

    Se eliminó código comentado

--- resources/views/pages/blog.blade.php
+++ resources/views/pages/blog.blade.php
@@ -14,7 +14,6 @@
         <div class="px-10 md:px-0 flex flex-col items-center">
             <section>
                 <div class="grid gap-10 lg:grid-cols-3">
-{{--                    <x-blog.articulo-descargas />--}}
                     @foreach($posts as $post)
                         <x-blog.articulo-card :post="$post" />
                     @endforeach
